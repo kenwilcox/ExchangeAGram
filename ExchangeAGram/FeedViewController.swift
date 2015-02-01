2718b2aa09b2847301a36c5f92710323b37aaf3b
kenwilcox <wilcoxk@gmail.com>
2015-02-01 00:53:27 -0700

    attempting to get an image when camera button pressed

--- ExchangeAGram/FeedViewController.swift
+++ ExchangeAGram/FeedViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class FeedViewController: UIViewController {
   
@@ -23,6 +24,35 @@ class FeedViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
+  @IBAction func snapBarButtonItemTapped(sender: UIBarButtonItem) {
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+      var cameraController = UIImagePickerController()
+      cameraController.delegate = self
+      cameraController.sourceType = UIImagePickerControllerSourceType.Camera
+      
+      let mediaTypes:[AnyObject] = [kUTTypeImage]
+      cameraController.mediaTypes = mediaTypes
+      cameraController.allowsEditing = false
+      
+      self.presentViewController(cameraController, animated: true, completion: nil)
+    }
+    else if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+      var photoLibraryController = UIImagePickerController()
+      photoLibraryController.delegate = self
+      photoLibraryController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+      
+      let mediaTypes:[AnyObject] = [kUTTypeImage]
+      photoLibraryController.mediaTypes = mediaTypes
+      photoLibraryController.allowsEditing = false
+      
+      self.presentViewController(photoLibraryController, animated: true, completion: nil)
+    }
+    else {
+      var alertView = UIAlertController(title: "Alert", message: "Your device does not support the camera or photo Library", preferredStyle: UIAlertControllerStyle.Alert)
+      alertView.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+      self.presentViewController(alertView, animated: true, completion: nil)
+    }
+  }
   
   /*
   // MARK: - Navigation
@@ -47,11 +77,21 @@ extension FeedViewController: UICollectionViewDataSource {
   }
   
   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-    return UICollectionViewCell()
+    return collectionView.dequeueReusableCellWithReuseIdentifier("feedCell", forIndexPath: indexPath) as FeedCell
   }
 }
 
 // MARK: - UICollectionViewDelegate
 extension FeedViewController: UICollectionViewDelegate {
+  
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension FeedViewController: UIImagePickerControllerDelegate {
+  
+}
+
+// MARK: - UINavigationControllerDelegate
+extension FeedViewController: UINavigationControllerDelegate {
   
 }
