f02fafddc39a8f2f47dad7dc851e3659d667c220
kenwilcox <wilcoxk@gmail.com>
2015-02-01 00:23:56 -0700

    Setup Feed Cell

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Kenneth Wilcox on 2/1/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var captionLabel: UILabel!
+}
