f07e471114c6544f572b225a6138054a1c272e87
kenwilcox <wilcoxk@gmail.com>
2015-02-03 00:46:34 -0700

    Starting to add some filters

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -12,6 +12,7 @@ class FilterViewController: UIViewController {
   
   var thisFeedItem: FeedItem!
   var collectionView: UICollectionView!
+  let kIntensity = 0.7
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -34,6 +35,27 @@ class FilterViewController: UIViewController {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  func photoFilters() -> [CIFilter] {
+    let blur = CIFilter(name: "CIGaussianBlur")
+    let instant = CIFilter(name: "CIPhotoEffectInstant")
+    let noir = CIFilter(name: "CIPhotoEffectNoir")
+    let transfer = CIFilter(name: "CIPhotoEffectTransfer")
+    let unsharpen = CIFilter(name: "CIUnsharpMask")
+    let monochrome = CIFilter(name: "CIColorMonochrome")
+    
+    let colorControls = CIFilter(name: "CIColorControls")
+    colorControls.setValue(0.5, forKey: kCIInputSaturationKey)
+    
+    let sepia = CIFilter(name: "CISepiaTone")
+    sepia.setValue(kIntensity, forKey: kCIInputIntensityKey)
+    
+    let colorClamp = CIFilter(name: "CIColorClamp")
+    colorClamp.setValue(CIVector(x: 0.9, y: 0.9, z: 0.9, w: 0.9), forKey: "inputMaxComponents")
+    colorClamp.setValue(CIVector(x: 0.2, y: 0.2, z: 0.2, w: 0.2), forKey: "inputMinComponents")
+    
+    return []
   }
 }
 
