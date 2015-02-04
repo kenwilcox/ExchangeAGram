ebec0dbca1896ab3417573efa0cfba36870f1e39
kenwilcox <wilcoxk@gmail.com>
2015-02-03 23:45:35 -0700

    Added thumbnail to FeedItem

--- ExchangeAGram/FeedItem.swift
+++ ExchangeAGram/FeedItem.swift
@@ -2,17 +2,18 @@
 //  FeedItem.swift
 //  ExchangeAGram
 //
-//  Created by Kenneth Wilcox on 2/2/15.
+//  Created by Kenneth Wilcox on 2/3/15.
 //  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-@objc (FeedItem)
+@objc(FeedItem)
 class FeedItem: NSManagedObject {
 
-    @NSManaged var caption: String
-    @NSManaged var image: NSData
+  @NSManaged var caption: String
+  @NSManaged var image: NSData
+  @NSManaged var thumbnail: NSData
 
 }
