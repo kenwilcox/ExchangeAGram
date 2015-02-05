6897c807dda0348f30dcc1175e4c5f2407f740ca
kenwilcox <wilcoxk@gmail.com>
2015-02-05 07:58:36 -0700

    Added a caption label

--- ExchangeAGram/FilterCell.swift
+++ ExchangeAGram/FilterCell.swift
@@ -11,11 +11,16 @@ import UIKit
 class FilterCell: UICollectionViewCell {
   
   var imageView: UIImageView!
+  var captionLabel: UILabel!
   
   override init(frame: CGRect) {
     super.init(frame: frame)
     imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
     contentView.addSubview(imageView)
+    
+    captionLabel = UILabel(frame: CGRect(x: 14, y: 121, width: 123, height: 21))
+    captionLabel.textColor = UIColor.whiteColor()
+    contentView.addSubview(captionLabel)
   }
 
   required init(coder aDecoder: NSCoder) {
