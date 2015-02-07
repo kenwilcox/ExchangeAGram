f30c6be64de2c09de5ed43f77896245005700665
kenwilcox <wilcoxk@gmail.com>
2015-02-07 12:43:54 -0700

    Added Bridging-Header.h

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Kenneth Wilcox on 2/7/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+#ifndef ExchangeAGram_Bridging_Header_h
+#define ExchangeAGram_Bridging_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
