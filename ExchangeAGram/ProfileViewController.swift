2d9b33a8f82b9d5c1a89337478d41a99aa1bd8d9
kenwilcox <wilcoxk@gmail.com>
2015-02-07 18:06:12 -0700

    Facebook LoginViewDelegate and permissions

--- ExchangeAGram/ProfileViewController.swift
+++ ExchangeAGram/ProfileViewController.swift
@@ -18,6 +18,8 @@ class ProfileViewController: UIViewController {
     super.viewDidLoad()
     
     // Do any additional setup after loading the view.
+    self.fbLoginView.delegate = self
+    self.fbLoginView.readPermissions = ["public_profile", "publish_actions"]
   }
   
   override func didReceiveMemoryWarning() {
@@ -25,15 +27,36 @@ class ProfileViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
+}
+
+extension ProfileViewController: FBLoginViewDelegate {
   
-  /*
-  // MARK: - Navigation
-  
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-  // Get the new view controller using segue.destinationViewController.
-  // Pass the selected object to the new view controller.
+  func loginViewShowingLoggedInUser(loginView: FBLoginView!) {
+    profileImageView.hidden = false
+    nameLabel.hidden = false
   }
-  */
+  
+  func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+    println("loginViewFetchUserInfo: \(user)")
+    
+    nameLabel.text = user.name
+    
+//    let userImageURL = "https://graph.facebook.com/\(user.objectID)/picture?type=small"
+//    let url = NSURL(string: userImageURL)
+//    let imageData = NSData(contentsOfURL: url!)
+//    let image = UIImage(data: imageData!)
+//    profileImageView.image = image
+    
+    profileImageView.image = UIImage(data: NSData(contentsOfURL: NSURL(string: "https://graph.facebook.com/\(user.objectID)/picture?type=normal")!)!)
+  }
+  
+  func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
+    profileImageView.hidden = true
+    nameLabel.hidden = true
+  }
+  
+  func loginView(loginView: FBLoginView!, handleError error: NSError!) {
+    println("Error: \(error.localizedDescription)")
+  }
   
 }
